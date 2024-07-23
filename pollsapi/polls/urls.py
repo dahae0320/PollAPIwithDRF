e74c788e7b440c2fe94191f97d5050e3a0a5abb0
dadahye <ldh990320ldh@gmail.com>
2024-07-23 14:56:57 +0900

    Refactor: 사용하지 않는 디펜던시 삭제 - rest_framework.auththoken의 views 삭제

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken import views
 from .apiview import PollViewSet, ChoiceList, CreateVote, UserListCreate, LoginView
 
 
